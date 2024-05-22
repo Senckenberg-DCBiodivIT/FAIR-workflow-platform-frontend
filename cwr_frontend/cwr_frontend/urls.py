a8dc0924506f7039f246e29bf50a179163615317
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-05-22 11:52:02 +0200

    add imprint

--- cwr_frontend/cwr_frontend/urls.py
+++ cwr_frontend/cwr_frontend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.urls import path, re_path
 from django.conf import settings
+from django.views.generic import TemplateView
 from revproxy.views import ProxyView
 
 from cwr_frontend.views.DatasetListView import DatasetListView
@@ -27,6 +28,7 @@ urlpatterns = [
     re_path(r'^/?$', DatasetListView.as_view(), name="dataset_list"),
     # regex for dataset ids with forward slash (id=prefix/object_id)
     re_path(r'dataset/(?P<id>[a-z0-9\/]+)', DatasetDetailView.as_view(), name="dataset_detail"),
+    path('imprint', TemplateView.as_view(template_name="imprint.html"), name="imprint"),
     # proxy all api requests to cordra
     re_path(r'api/(?P<path>.*)', ProxyView.as_view(upstream=settings.CORDRA["URL"]), name="api"),
     # an ugly hack to serve a placeholder image without using static files
