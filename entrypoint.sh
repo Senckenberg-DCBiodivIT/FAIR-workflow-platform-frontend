f0835507fd84f0657bc9e604707cb232324c6b23
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-06-02 14:49:58 +0200

    increase gunicorn timeout and worker count

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,5 @@ python manage.py collectstatic --no-input --clear
 echo "Apply database migrations"
 python manage.py migrate --noinput
 
-# running command
-echo "running $@"
-gunicorn cwr_frontend.wsgi:application --bind 0.0.0.0:8000
+# running gunicorn. Increased timeout due to ro crate zip parsing
+gunicorn cwr_frontend.wsgi:application --bind 0.0.0.0:8000 --timeout ${GUNICORN_TIMEOUT:-120} --workers ${GUNICORN_WORKERS:-2}
