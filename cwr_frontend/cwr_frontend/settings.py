1ff6fde65f9094176eca5e6f663cb89bd6d0b14d
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-08-02 11:42:00 +0200

    add link to object repository

--- cwr_frontend/cwr_frontend/settings.py
+++ cwr_frontend/cwr_frontend/settings.py
@@ -89,6 +89,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'settings_value': 'cwr_frontend.templatetags.settings_value',
+            }
         },
     },
 ]
