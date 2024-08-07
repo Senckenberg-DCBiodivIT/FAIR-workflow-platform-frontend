66096865a430f7dbf3c423509912edf370ef963f
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-08-07 15:00:14 +0200

    refactor workflow submission form

--- cwr_frontend/cwr_frontend/views/WorkflowsView.py
+++ cwr_frontend/cwr_frontend/views/WorkflowsView.py
@@ -20,61 +20,40 @@ class WorkflowsView(TemplateView):
     _logger = logging.getLogger(__name__)
     _connector = WorkflowServiceConnector()
 
-    def get(self, request, **kwargs):
-        step = kwargs.get("step", 1)
-        context = {}
-        try:
-            social_account = SocialAccount.objects.get(user=request.user, provider="orcid")
-            context["username"] = f"{social_account.user.first_name.title()} {social_account.user.last_name.title()}"
-            context["orcid"] = social_account.uid
-        except SocialAccount.DoesNotExist:
-            raise Exception("User has no ORCID")
+    def handle_request(self, request, **kwargs):
+        if request.method == "POST":
+            if "rocratefile" in request.FILES:
+                return self.step_2(request)
+            elif "cancel" in request.POST:
+                request.session = {}
+                return self.step_1(request)
+            elif "submit" in request.POST and "workflow" in request.session:
+                return self.step_3(request)
+        return self.step_1(request)
 
-        if step == 2:
-            context["workflow"] = yaml.dump(request.session.get("workflow", None))
-            if not request.session.get("workflow_check_status", True):
-                context["workflow_check_result"] = request.session.get("workflow_check_result")
-        elif step == 3:
-            if not request.session.get("submit_status", True):
-                context["submit_result"] = request.session.get("submit_result")
+    def step_1(self, request):
+        self._logger.info("Render step 1")
+        context = {"step": 1}
+        return render(request, self.template_name, context=context)
 
-        context["step"] = step
-        return render(request, self.template_name, context)
-
-    def post(self, request, **kwargs):
-        if "rocratefile" in request.FILES:
-            return self.handle_crate_upload(request)
-        elif "submit" in request.POST:
-            social_acc = SocialAccount.objects.get(user=request.user, provider="orcid")
-            name = social_acc.user.first_name.title() + " " + social_acc.user.last_name.title()
-            orcid = social_acc.uid
-            submit_status, submit_result = self._connector.submit_workflow(request.session["workflow"], submitter_name=name, submitter_orcid=orcid, dry_run=request.POST.get("dryrun", None)  == "DryRun")
-            request.session["workflow"] = None
-            request.session["submit_status"] = submit_status
-            request.session["submit_result"] = submit_result
-            return self.get(request, step=3)
-        elif "cancel" in request.POST:
-            request.session["workflow"] = None
-            return self.get(request, step=1)
-        else:
-            return self.get(request)
-
-    def handle_crate_upload(self, request):
+    def step_2(self, request):
+        self._logger.info("Render step 2")
         file = request.FILES["rocratefile"]
 
         # check if this is a zip file
         if (file.content_type != "application/zip"):
-
             raise BadRequest("File is not a zip file")
+
+        # Parse RO crate and extract workflow
         with file as f:
-            # check if zip is broken or not valid zip file
+            # assert zip file is valid
             try:
                 bytes = io.BytesIO(f.read())
                 zipfile.ZipFile(bytes)
             except zipfile.BadZipFile as e:
                 raise BadRequest("File is not a zip file")
 
-            # parse ro crate and find workflow file
+            # parse ro-crate and find workflow file
             f.seek(0)
             with tempfile.NamedTemporaryFile(delete=True) as tmp:
                 for chunk in file.chunks():
@@ -84,12 +63,52 @@ class WorkflowsView(TemplateView):
                 crate = ROCrate(tmp.name)
                 workflow_path = crate.source / crate.root_dataset["mainEntity"]["name"]
 
+                # check workflow file and set it to the session
                 if workflow_path.exists():
                     workflow = yaml.load(open(workflow_path, "r"), Loader=yaml.CLoader)
-                    workflow_lint_status, workflow_lint_result = self._connector.check_workflow(workflow)
-                    # TODO supply parameters for workflow based on formal parameters?
-                    request.session["workflow_check_status"] = workflow_lint_status
-                    request.session["workflow_check_result"] = workflow_lint_result
-                    request.session["workflow"] = workflow
-                    return self.get(request, step=2)
+                else:
+                    raise Exception("Workflow file not found in RO-Crate")
+
+        # check if workflow is valid
+        workflow_lint_status, workflow_lint_result = self._connector.check_workflow(workflow)
+
+        # Update session with workflow
+        request.session["workflow"] = workflow
+        request.session["workflow_check_status"] = workflow_lint_status
+        request.session["workflow_check_result"] = workflow_lint_result
+
+        social_account = SocialAccount.objects.get(user=request.user, provider="orcid")
+        context = {
+            "step": 2,
+            "workflow": yaml.dump(workflow, indent=2),
+            "username": f"{social_account.user.first_name.title()} {social_account.user.last_name.title()}",
+            "orcid": social_account.uid,
+        }
+        if not workflow_lint_status:
+            context["workflow_error"] = workflow_lint_result
+        return render(request, self.template_name, context=context)
+
+    def step_3(self, request):
+        self._logger.info("Render step 3")
+        social_acc = SocialAccount.objects.get(user=request.user, provider="orcid")
+        name = social_acc.user.first_name.title() + " " + social_acc.user.last_name.title()
+        orcid = social_acc.uid
+
+        # submit workflow to workflow service
+        submit_status, submit_result = self._connector.submit_workflow(request.session["workflow"], submitter_name=name, submitter_orcid=orcid, dry_run=request.POST.get("dryrun", None)  == "DryRun")
+        del request.session["workflow"]
+
+        context = {
+            "step": 3,
+            "submit_status": submit_status,
+            "submit_result": submit_result
+        }
+        return render(request, self.template_name, context)
+
+
+    def get(self, request, **kwargs):
+        return self.handle_request(request, **kwargs)
+
+    def post(self, request, **kwargs):
+        return self.handle_request(request, **kwargs)
 
