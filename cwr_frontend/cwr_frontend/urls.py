7057c38219c666510dbcb58269ef802553429996
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-08-21 13:30:17 +0200

    style login

--- cwr_frontend/cwr_frontend/urls.py
+++ cwr_frontend/cwr_frontend/urls.py
@@ -15,14 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib.auth.decorators import login_required
-from django.urls import re_path, path, include
+from django.urls import re_path, path, include, reverse
 from django.views.generic import TemplateView
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 from cwr_frontend.views.WorkflowListView import WorkflowListView
 from cwr_frontend.views.WorkflowSubmissionView import WorkflowSubmissionView
 from cwr_frontend.views.DatasetListView import DatasetListView
 from cwr_frontend.views.DatasetDetailView import DatasetDetailView
+
+def redirect_orcid_login(request):
+    next_url = request.GET.get("next", "/")
+    orcid_login_url = reverse("orcid_login") + "?process=login&next=" + next_url
+    return HttpResponseRedirect(orcid_login_url)
 
 urlpatterns = [
     # browser reload in debug mode
@@ -37,6 +43,6 @@ urlpatterns = [
     # an ugly hack to serve a placeholder image without using static files
     # replace with actual static file servement!
     path('admin/', admin.site.urls),
+    path("accounts/login/", redirect_orcid_login, name="account_login"), # skip login selection page -> always orcid
     path("accounts/", include("allauth.urls")),
-    path("accounts/profile/", TemplateView.as_view(template_name="profile.html")),
 ]
