c70db90cc7d53bfda84f1201176ff56fae6ff201
Daniel Bauer <daniel.bauer@senckenberg.de>
2024-11-29 18:06:13 +0100

    refactored tests

--- cwr_frontend/cwr_frontend/tests/test_ro_crate.py
+++ cwr_frontend/cwr_frontend/tests/test_ro_crate.py
@@ -15,89 +15,7 @@ def compare_dicts(expected, actual):
     for key in actual:
         assert key in expected
 
-def test_remote_ro_crate():
-    """"
-    Test RO crate with remote objects
-    """
-    dataset_objects = json.load(open(os.path.join(os.path.dirname(__file__), "dataset_objects_workflow.json"), "r"))
-    dataset_id = "cwr/2b408313359f2b6f18c2"
-    remote_urls = {}
-    for id, object in dataset_objects.items():
-        if "MediaObject" in object["@type"] or "Person" in object["@type"]:
-            remote_urls[id] = "https://example.com/" + id
-
-    ro_crate = rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        ro_crate.write(tmp_dir)
-        settings = services.ValidationSettings(
-            data_path=tmp_dir,
-            profile_identifier="workflow-run-crate-0.5",
-            requirement_severity=models.Severity.REQUIRED,
-            abort_on_first=False
-        )
-        result = services.validate(settings)
-        if result.has_issues():
-            for issue in result.get_issues():
-                # Ignore missing workflow. It can't be found because it is an empty example crate.
-                if re.search("Main Workflow .*? not found in crate", issue.message):
-                    continue
-                else:
-                    raise AssertionError("Crate validation failed: " + str(issue))
-
-    assert ro_crate.root_dataset["@id"] == "./"
-
-    assert ro_crate.dereference("https://orcid.org/0000-0001-9447-460X") is not None
-    assert ro_crate.dereference("https://example.com/cwr/695601f5a098bf326246") is not None
-    assert ro_crate.dereference("https://example.com/cwr/0d6b270d30e33a451e64") is not None
-    assert ro_crate.dereference("https://example.com/cwr/00ff7fa98ac0694e0f20") is not None
-
-    assert ro_crate.root_dataset["mainEntity"]["@id"] == "https://example.com/cwr/695601f5a098bf326246"
-    assert [entity["@id"] for entity in ro_crate.root_dataset["hasPart"]] == [
-        "https://example.com/cwr/0d6b270d30e33a451e64",
-        "https://example.com/cwr/00ff7fa98ac0694e0f20",
-        "https://example.com/cwr/695601f5a098bf326246",
-    ]
-    assert ro_crate.root_dataset["mentions"]["@id"].startswith("#cwr")
-
-    action = ro_crate.root_dataset["mentions"]
-    assert action["instrument"]["@id"] == "https://example.com/cwr/695601f5a098bf326246"
-    assert action["agent"]["@id"] == "https://orcid.org/0000-0001-9447-460X"
-    assert [entity["@id"] for entity in action["result"]] == [
-        "https://example.com/cwr/0d6b270d30e33a451e64",
-        "https://example.com/cwr/00ff7fa98ac0694e0f20",
-    ]
-
-
-def test_remote_ro_crate_missing_urls():
-    """
-    Should raise an error if a remote url to a file is missing, but not if a person url is missing
-    """
-    dataset_objects = json.load(open(os.path.join(os.path.dirname(__file__), "dataset_objects_workflow.json"), "r"))
-    dataset_id = "cwr/2b408313359f2b6f18c2"
-    remote_urls = {}
-
-    # should throw without remote urls for a file
-    with pytest.raises(ValueError):
-        rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
-
-    # should not throw for missing remote url for a person
-    for id, object in dataset_objects.items():
-        if "MediaObject" in object["@type"]:  # not for person!
-            remote_urls[id] = "https://example.com/" + id
-
-    ro_crate = rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
-    assert ro_crate is not None
-
-    author = ro_crate.root_dataset["author"]
-    compare_dicts({
-        "@id": "https://orcid.org/0000-0001-9447-460X",
-        "@type": "Person",
-        "name": "Daniel Bauer"
-    }, author.as_jsonld())
-
-
-def test_file_based_ro_crate():
+def test_workflow_ro_crate():
     """
     Test RO crate with file based objects
     """
@@ -220,15 +138,26 @@ def test_file_based_ro_crate():
         "value": "test parameter"
     }, action_property.as_jsonld())
 
-def test_ro_crate_without_workflow():
 
+def test_workflow_ro_crate_with_child():
+    raise NotImplementedError()
+
+
+def test_ro_crate_with_parent():
+    # Make sure this crate references its parents crate for provenance
+    # assert "isPartOf" in ro_crate.root_dataset
+    # assert ro_crate.root_dataset["isPartOf"] == "https://example.com/cwr/d1d35fd05b740bb3c393?format=ROCrate"
+    raise NotImplementedError()
+
+
+def test_ro_crate():
     """
     Test RO crate with file based objects that does not contain a workflow.
     It should conform to Ro-Crate but not include the WRROC profile
     """
     dataset_objects = json.load(open(os.path.join(os.path.dirname(__file__), "dataset_objects.json"), "r"))
     dataset_id = "cwr/c4e56be8890a3fda2fac"
-    remote_urls = {"cwr/d1d35fd05b740bb3c393": "https://example.com/cwr/d1d35fd05b740bb3c393?format=ROCrate"}
+    remote_urls = {}
     for id, object in dataset_objects.items():
         if "MediaObject" in object["@type"] or "Person" in object["@type"]:
             remote_urls[id] = "https://example.com/" + id
@@ -258,6 +187,96 @@ def test_ro_crate_without_workflow():
     assert ro_crate.metadata["conformsTo"] == "https://w3id.org/ro/crate/1.1"
     assert len(ro_crate.metadata.extra_contexts) == 0
 
-    # Make sure this crate references its parents crate for provenance
-    assert "isPartOf" in ro_crate.root_dataset
-    assert ro_crate.root_dataset["isPartOf"] == "https://example.com/cwr/d1d35fd05b740bb3c393?format=ROCrate"
+
+def test_detached_workflow_ro_crate():
+    """"
+    Test RO crate with remote objects
+    """
+    dataset_objects = json.load(open(os.path.join(os.path.dirname(__file__), "dataset_objects_workflow.json"), "r"))
+    dataset_id = "cwr/2b408313359f2b6f18c2"
+    remote_urls = {dataset_id: "https://example.com/" + dataset_id}
+    for id, object in dataset_objects.items():
+        if "MediaObject" in object["@type"] or "Person" in object["@type"]:
+            remote_urls[id] = "https://example.com/" + id
+
+    ro_crate = rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        ro_crate.write(tmp_dir)
+        from pprint import pprint
+        pprint(json.load(open(os.path.join(tmp_dir, "ro-crate-metadata.json"),  "r")))
+        settings = services.ValidationSettings(
+            data_path=tmp_dir,
+            profile_identifier="workflow-run-crate-0.5",
+            requirement_severity=models.Severity.REQUIRED,
+            abort_on_first=False
+        )
+        result = services.validate(settings)
+        if result.has_issues():
+            for issue in result.get_issues():
+                # Ignore missing workflow. It can't be found because it is an empty example crate.
+                if re.search("Main Workflow .*? not found in crate", issue.message):
+                    continue
+                else:
+                    raise AssertionError("Crate validation failed: " + str(issue))
+
+    # Root dataset should be referenced by its URI instead of ./
+    assert ro_crate.root_dataset["@id"] == remote_urls[dataset_id]
+
+    assert ro_crate.dereference("https://orcid.org/0000-0001-9447-460X") is not None
+    assert ro_crate.dereference("https://example.com/cwr/695601f5a098bf326246") is not None
+    assert ro_crate.dereference("https://example.com/cwr/0d6b270d30e33a451e64") is not None
+    assert ro_crate.dereference("https://example.com/cwr/00ff7fa98ac0694e0f20") is not None
+
+    assert ro_crate.root_dataset["mainEntity"]["@id"] == "https://example.com/cwr/695601f5a098bf326246"
+    assert [entity["@id"] for entity in ro_crate.root_dataset["hasPart"]] == [
+        "https://example.com/cwr/0d6b270d30e33a451e64",
+        "https://example.com/cwr/00ff7fa98ac0694e0f20",
+        "https://example.com/cwr/695601f5a098bf326246",
+    ]
+    assert ro_crate.root_dataset["mentions"]["@id"].startswith("#cwr")
+
+    action = ro_crate.root_dataset["mentions"]
+    assert action["instrument"]["@id"] == "https://example.com/cwr/695601f5a098bf326246"
+    assert action["agent"]["@id"] == "https://orcid.org/0000-0001-9447-460X"
+    assert [entity["@id"] for entity in action["result"]] == [
+        "https://example.com/cwr/0d6b270d30e33a451e64",
+        "https://example.com/cwr/00ff7fa98ac0694e0f20",
+    ]
+
+
+def test_detached_workflow_ro_crate_missing_urls():
+    """
+    Should raise an error if a remote url to a file is missing, but not if a person url is missing
+    """
+    dataset_objects = json.load(open(os.path.join(os.path.dirname(__file__), "dataset_objects_workflow.json"), "r"))
+    dataset_id = "cwr/2b408313359f2b6f18c2"
+    remote_urls = {}
+
+    # should throw without remote urls for a file
+    with pytest.raises(ValueError):
+        rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
+
+    # should not throw for missing remote url for a person
+    for id, object in dataset_objects.items():
+        if "MediaObject" in object["@type"]:  # not for person!
+            remote_urls[id] = "https://example.com/" + id
+
+    ro_crate = rocrate_utils.build_ROCrate(dataset_id, dataset_objects, remote_urls=remote_urls, with_preview=False, detached=False)
+    assert ro_crate is not None
+
+    author = ro_crate.root_dataset["author"]
+    compare_dicts({
+        "@id": "https://orcid.org/0000-0001-9447-460X",
+        "@type": "Person",
+        "name": "Daniel Bauer"
+    }, author.as_jsonld())
+
+
+def test_detached_workflow_ro_crate_with_child():
+    """ Should correcly reference the child urls """
+    raise NotImplementedError()
+
+
+def test_detached_ro_crate_with_parent():
+    raise NotImplementedError()
