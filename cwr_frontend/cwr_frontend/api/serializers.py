e0307cf1241f96576422d702572abf410b146016
Lena Perzlmaier <lena.perzlmaier@senckenberg.de>
2025-09-03 10:12:51 +0200

    feat: progress on API logic

--- cwr_frontend/cwr_frontend/api/serializers.py
+++ cwr_frontend/cwr_frontend/api/serializers.py
@@ -3,5 +3,11 @@
 from rest_framework import serializers
 
 class WorkflowStatusSerializer(serializers.Serializer):
-    workflow_id = serializers.UUIDField()
     status = serializers.CharField()
+    workflow_id = serializers.UUIDField(required = False)
+    details = serializers.DictField(required = False)
+
+
+class WorkflowSubmissionSerializer(serializers.Serializer):
+    rocratefile = serializers.FileField(required=True )
+    dry_run = serializers.BooleanField(required = False, default = False)
--- cwr_frontend/cwr_frontend/api/views.py
+++ cwr_frontend/cwr_frontend/api/views.py
@@ -1,17 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import FileResponse
+from django.http import Http404, StreamingHttpResponse
+from django.conf import settings
 from drf_spectacular.utils import extend_schema
+from requests import HTTPError
 
-from uuid import uuid4
-
-from cwr_frontend.rocrate_utils import get_crate_workflow_from_zip
+from cwr_frontend.rocrate_utils import get_crate_workflow_from_zip, as_ROCrate
 from cwr_frontend.workflowservice.WorkflowServiceConnector import WorkflowServiceConnector
-from cwr_frontend.api.serializers import WorkflowStatusSerializer
+from cwr_frontend.cordra.CordraConnector import CordraConnector
+from cwr_frontend.api.serializers import WorkflowStatusSerializer, WorkflowSubmissionSerializer
 from rest_framework_api_key.permissions import HasAPIKey
 
-def workflow_status_response(workflow_id, status, status_code=200):
-    data = {"workflow_id": workflow_id, "status": status}
+def workflow_status_response(status, workflow_id = None, details = None, status_code=200):
+    data = { "status": status}
+    if workflow_id is not None:
+        data['workflow_id'] = workflow_id  
+    if details is not None:
+        data['details'] = details    
     serializer = WorkflowStatusSerializer(data)
     return Response(serializer.data, status=status_code)
 
@@ -20,25 +25,26 @@ class SubmitWorkflowView(APIView):
 
     _connector = WorkflowServiceConnector()
     permission_classes = [HasAPIKey]
-    
+
     @extend_schema(
-        responses={200: WorkflowStatusSerializer}
+        request=WorkflowSubmissionSerializer,
+        responses={200: WorkflowStatusSerializer},
     )
     def post(self, request):
-        workflow_id = str(uuid4())
-        file = request.FILES.get('rocratefile')
-        dry_run = request.query_params.get('dry_run')
+        serializer = WorkflowSubmissionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        crate, workflow = get_crate_workflow_from_zip(file = file)
+        file = serializer.validated_data['rocratefile']
+        dry_run = serializer.validated_data['dry_run']
+
+        _, workflow = get_crate_workflow_from_zip(file = file)
 
         # check if workflow is valid
         workflow_lint_status, workflow_lint_result = self._connector.check_workflow(workflow)
 
         if not workflow_lint_status:
-            # Or better return BadRequest?
-            # Migh be useful to include result/details here
-            status = "Invalid ROCrate"
-            return workflow_status_response(workflow_id, status)
+            status = "Invalid workflow"
+            return workflow_status_response(status, workflow_id, workflow_lint_result)
         
         override_parameters = {}
         for key, value in request.POST.items():
@@ -48,43 +54,62 @@ class SubmitWorkflowView(APIView):
         
         submit_status, submit_result = self._connector.submit_workflow(
             workflow=workflow,
-            title=workflow_id,
+            title='empty_title',
             description="empty description",
             # keywords=request.POST["keywords"].split(","),
-            # license=request.POST["license"],
+            license='https://spdx.org/licenses/MIT',
             override_parameters=override_parameters,
             submitter_name='Lena',
-            submitter_orcid='123',
+            submitter_orcid='0000-0002-0487-2151',
             dry_run=dry_run,
         )
 
         if not submit_status:
-            status = "Failed"
-            return workflow_status_response(workflow_id, status)
+            status = "Submission Failed"
+            return workflow_status_response(status, details = submit_result)
+        
+        workflow_id = submit_result['workflow_id']
 
         if dry_run:
             status = "Valid Request"            
-            return workflow_status_response(workflow_id, status)
+            return workflow_status_response(status, workflow_id)
         
         status = "Submitted"        
-        return workflow_status_response(workflow_id, status)
+        return workflow_status_response(status, workflow_id)
 
 class WorkflowStatusView(APIView):
+    _connector = WorkflowServiceConnector()
     @extend_schema(
         responses={200: WorkflowStatusSerializer}
     )
+
     def get(self, request, workflow_id):
 
-        status = 'placeholder'
+        wfl = self._connector.get_workflow_detail(workflow_id)
+        status = wfl['status']
 
-        return workflow_status_response(workflow_id, status)
+        return workflow_status_response(status, workflow_id)
 
 
 class WorkflowDownloadView(APIView):
-    def get(self, request, workflow_id):
+    def __init__(self, prefix=settings.CORDRA["PREFIX"], user=settings.CORDRA["USER"], password=settings.CORDRA["PASSWORD"]):
+        self.user = user
+        self.password = password
+        self.prefix = prefix
+        self._connector = CordraConnector(user = user, password=password)
 
-        file_path = '/home/lena/Downloads/cwr_0ea7316aa449cf745efc.zip'
-        response = FileResponse(open(file_path, 'rb'), content_type='application/octet-stream')
-        response['Content-Disposition'] = 'attachment; filename="ro-crate.zip"'
+    def get(self, request, workflow_id)->StreamingHttpResponse:
+        workflow_id = f"{self.prefix}/{workflow_id}"
+        try:
+            object = self._connector.get_object_by_id(workflow_id)
+            print(object)
 
-        return response
+        except HTTPError as e:
+            # Cordra responds with 401 if not a public object is not found.
+            if e.response.status_code == 401 or e.response.status_code == 404:
+                raise Http404
+            raise
+
+        objects = self._connector.resolve_objects(workflow_id, nested=False, workflow_only=False)
+        print(objects)
+        return as_ROCrate(request, workflow_id, objects, download=True, connector=self._connector)
