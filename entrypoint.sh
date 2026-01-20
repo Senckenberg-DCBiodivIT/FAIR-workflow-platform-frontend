6dad33955a6111612b70fbb09c9736944c75f592
Lena Perzlmaier <lena.perzlmaier@senckenberg.de>
2026-01-20 15:12:36 +0100

    refactor: streamline entrypoint script by removing .env loading and dynamically defining the port

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ -f /app/cwr_frontend/.env ]; then
-  export $(grep -v '^#' /app/cwr_frontend/.env | xargs)
-fi
-
 if [ "$USE_POSTGRES" = "TRUE" ]; then
-  # Wait for Postgres
   echo "Waiting for Postgres at $POSTGRES_HOST:$POSTGRES_PORT..."
   until nc -z $POSTGRES_HOST $POSTGRES_PORT; do
     sleep 1
@@ -14,17 +9,18 @@ if [ "$USE_POSTGRES" = "TRUE" ]; then
   echo "Postgres is up - continuing..."
 fi
 
-
 echo "Collect static files"
 python manage.py collectstatic --no-input --clear
 
 echo "Apply database migrations"
 python manage.py migrate --noinput
 
-# running gunicorn. Increased timeout due to ro crate zip parsing
-gunicorn cwr_frontend.wsgi:application \
-    --bind 0.0.0.0:${FRONTEND_PORT:-8000}  \
+FRONTEND_PORT=${FRONTEND_PORT:-8000}
+echo "Starting Gunicorn on port $FRONTEND_PORT..."
+
+exec gunicorn cwr_frontend.wsgi:application \
+    --bind 0.0.0.0:$FRONTEND_PORT \
     --timeout ${GUNICORN_TIMEOUT:-120} \
     --workers ${GUNICORN_WORKERS:-2} \
     --threads ${GUNICORN_THREADS:-5} \
-    --access-logfile -  
+    --access-logfile -
